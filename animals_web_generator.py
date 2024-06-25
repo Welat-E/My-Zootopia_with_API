102bc120efc5a02bfaf6c0dcb7062a89bb4f9623
Welat-E <welaterenug@gmail.com>
2024-06-25 12:40:58 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,19 +5,30 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
+# Daten aus der JSON-Datei lesen
+animals_data = load_data('animals_data.json') 
 
+# HTML-Code für die Tierkarten generieren (mit angepasster Einrückung)
+html_output = ""
 for animal in animals_data:
-    print()
+    html_output += '        <li class="cards__item">\n'  # 8 Leerzeichen Einrückung
     if "name" in animal:
-        print("Name:", animal["name"])
+        html_output += f'            <h2>{animal["name"]}</h2>\n'  # 12 Leerzeichen Einrückung
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        print("Diet:", animal["characteristics"]["diet"])
+        html_output += f'            <p>Diet: {animal["characteristics"]["diet"]}</p>\n'
     if "locations" in animal and animal["locations"]:
-        print("Location:", animal["locations"][0])
-
-    # Suche nach "type" im Haupteintrag, falls nicht vorhanden, dann in "characteristics"
+        html_output += f'            <p>Location: {animal["locations"][0]}</p>\n'
     if "type" in animal:
-        print("Type:", animal["type"])
+        html_output += f'            <p>Type: {animal["type"]}</p>\n'
     elif "characteristics" in animal and "type" in animal["characteristics"]:
-        print("Type:", animal["characteristics"]["type"])
+        html_output += f'            <p>Type: {animal["characteristics"]["type"]}</p>\n'
+    html_output += '        </li>\n'  # 8 Leerzeichen Einrückung
+
+# animals_template.html einlesen und Platzhalter ersetzen
+with open('animals_template.html', 'r') as f:
+    html_template = f.read()
+html_output = html_template.replace('__REPLACE_ANIMALS_INFO__', html_output)
+
+# Neuen HTML-Code in die Datei animals.html schreiben
+with open('animals.html', 'w') as f:
+    f.write(html_output)
